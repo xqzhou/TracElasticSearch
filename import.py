7aac61f90e11fff6086bbe7ed9a71d9ab4dc2bad
Xiaoqiang Zhou <xiaoqiang.chou@gmail.com>
2013-02-01 22:24:19 +0800

    script to index existing wiki

--- import.py
+++ import.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from sqlite3 import connect, Row
 from pyelasticsearch import ElasticSearch
 
@@ -6,12 +7,26 @@ es = ElasticSearch('http://localhost:9200/')
 
 conn = connect('/Users/xq/Workspace/my-trac-proj/db/trac.db')
 conn.row_factory = Row
+
+def index_wiki(r): 
+	doc = {}
+	doc['name'] = r['name']
+	doc['comment'] = r['comment']
+	doc['author'] = r['author']
+	doc['text'] = r['text']
+	doc['version'] = r['version']
+	doc['time'] = datetime.fromtimestamp(r['time'] / 1000000)
+	doc['ipnr'] = r['ipnr']
+	es.index('trac', 'wiki', doc, doc['name'])
+
 c = conn.cursor()
-c.execute('select * from wiki limit 1')
-r = c.fetchone()
+c.execute('select * from wiki')
+
+rows = c.fetchmany(50)
+while rows:
+	for r in rows:
+		index_wiki(r)
+	rows = c.fetchmany(50)
+
+c.close()
 conn.close()
-
-
-# doc = dict(zip(r.keys(), r))
-# es.index('trac', 'wiki', doc)
-
